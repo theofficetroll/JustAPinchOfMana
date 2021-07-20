8e922ae32e855822f338904b061364c38bcab80b
Mark <mark.scrudder@gmail.com>
2021-07-20 15:28:12 -0700

    Basic framework for ToolBuild component with notes.

---
+++ src/components/toolBuild.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// ToolBuild shows data for a specific tool:
+// How many there are, how much Mana they produce, and how much to purchase a new one.
+// Also allows for purchase of the tool
+
+// TODO
+// Show appropriate data
+// Add click to purchase
+// Visually indicate when Tool is purchaseable
+
+const ToolBuild = () => {
+  return (
+    <div>
+      WIP
+    </div>
+  )
+}
+
+export default ToolBuild;
